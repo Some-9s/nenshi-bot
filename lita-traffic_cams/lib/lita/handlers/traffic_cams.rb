176802602d66b1119085cfc78f014a75ddf149dd
Samson Nguyen <samson.nguyen@solium.com>
2015-04-24 22:30:34 -0600

    Update help

--- lita-traffic_cams/lib/lita/handlers/traffic_cams.rb
+++ lita-traffic_cams/lib/lita/handlers/traffic_cams.rb
@@ -2,7 +2,7 @@ module Lita
   module Handlers
     class TrafficCams < Handler
     
-      route(/What is the traffic like on\s(.+)/, command: true) do |response|
+      route(/What is the traffic like at\s(.+)/, command: true, help: {"What is the traffic like at [location]" => "Displays the traffic status of [location]"}) do |response|
         response.reply(response.matches)
       end
     end
