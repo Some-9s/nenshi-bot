bb3192d366ae53e4d0eba9241f703ee98dab5cc7
rwoolley <r.woolley@gmail.com>
2015-04-25 14:54:56 -0600

    me like hockey

--- lita-sports_equipment/lib/lita/handlers/sports_equipment.rb
+++ lita-sports_equipment/lib/lita/handlers/sports_equipment.rb
@@ -1,6 +1,16 @@
 module Lita
   module Handlers
     class SportsEquipment < Handler
+   		route(/^wanna play\s+(hockey|basketball|baseball)/, :sports, help: { "echo TEXT" => "Echoes back TEXT." })
+      def sports(response)
+        if response.matches[0] == "hockey"
+        	response.reply(response.matches)
+    	else
+    		response.reply("get outside!")
+    		response.reply(response.matches.inspect)
+    	end
+      end
+
     end
 
     Lita.register_handler(SportsEquipment)
