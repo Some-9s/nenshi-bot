c3de6fe40c44998ddd28e8a9380351b5abe3096f
Tom Dyer <tom.dyer@gmail.com>
2015-04-25 13:51:10 -0600

    Let me just take a selfie.

--- Gemfile
+++ Gemfile
@@ -11,4 +11,5 @@ gem "lita"
 # Add handlers to give Lita new functionality.
 # For example:
 gem "lita-google-images"
+gem "lita-selfie", :path => 'lita-selfie'
 #gem "lita-karma"
---
+++ lita-selfie/lib/lita/handlers/selfie.rb
@@ -0,0 +1,53 @@
+module Lita
+  module Handlers
+    class Selfie < Handler
+
+      URL = "https://ajax.googleapis.com/ajax/services/search/images"
+
+      route(/selfie\?/, :fetch, command: true, help: {
+        "How about a selfie?" => "Displays a random selfie from Google Images for Nenshi."
+      })
+
+      def fetch(response)
+        query = "nenshi selfie"
+
+        http_response = http.get(
+          URL,
+          v: "1.0",
+          q: query,
+          safe: :active,
+          rsz: 8
+        )
+
+        data = MultiJson.load(http_response.body)
+
+        if data["responseStatus"] == 200
+          choice = data["responseData"]["results"].sample
+          if choice
+            response.reply ensure_extension(choice["unescapedUrl"])
+          else
+            response.reply %{No images found for "#{query}".}
+          end
+        else
+          reason = data["responseDetails"] || "unknown error"
+          Lita.logger.warn(
+            "Couldn't get image from Google: #{reason}"
+          )
+        end
+      end
+
+      private
+
+      def ensure_extension(url)
+        if [".gif", ".jpg", ".jpeg", ".png"].any? { |ext| url.end_with?(ext) }
+          url
+        else
+          "#{url}#.png"
+        end
+      end
+
+    end
+
+    Lita.register_handler(Selfie)
+  end
+end
