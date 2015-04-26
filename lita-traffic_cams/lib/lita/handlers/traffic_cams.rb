d88d7a92964b67f132570613592b80672782ef94
Samson Nguyen <samson.nguyen@solium.com>
2015-04-25 19:53:00 -0600

    BigQuery on traffic cams

--- lita-traffic_cams/lib/lita/handlers/traffic_cams.rb
+++ lita-traffic_cams/lib/lita/handlers/traffic_cams.rb
@@ -3,56 +3,60 @@ module Lita
   module Handlers
     class TrafficCams < Handler
 
-      # $intersections = {"http://trafficcam.calgary.ca/loc34.jpg"}
-
-      $location_data = {"city hall" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "bow trail" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-                        "deerfoot" => {'url' => ['http://trafficcam.calgary.ca/loc53.jpg', 'http://trafficcam.calgary.ca/loc54.jpg', 'http://trafficcam.calgary.ca/loc56.jpg']},
-
-      }
-
       route(/What.+traffic\s+(like\s)?((at)|(on))\s(.+)[?]?/i, :traffic_at, command: false, help: {"What is the traffic like at [location]?" => "Displays the traffic status of [location]"})
 
       route(/What.+traffic\s+(like\s)?near me?/i, :traffic_at_city_hall, command: false, help: {"What is the traffic like near me?" => "Displays the traffic status of [location]"})
 
-      route(/where are all of the (available\s)?traffic cams[?]?/i, command: false, help: {"Where are all of the traffic cams?" => "Displays a map of the available traffic cams"}) do |response|
-        response.reply_with_mention "http://tinyurl.com/n39lkx2"
+      route(/((where)|(what)) are all of the (available\s)?traffic cams[?]?/i, command: false, help: {"Where are all of the traffic cams?" => "Displays a map of the available traffic cams"}) do |response|
+        response.reply_with_mention "Honestly, I think we need more!! http://tinyurl.com/n39lkx2"
       end
 
       def traffic_at_city_hall(response)
-        location_data = get_location_at(response, "city hall")
-
-        # return response.reply_with_mention("#{location_data.to_yaml} at #{location}")
-        location_data['url'].each do |url|
-          response.reply_with_mention(url)
-        end unless location_data.nil?
+        traffic_at(response, '11 Avenue')
       end
 
-      def traffic_at(response)
+      def traffic_at(response,location = nil)
+        reply = ''
+
         response.reply_with_mention("#{response.matches.to_json}\n\n")
-        location = response.matches[0][4]
+        location ||= response.matches[0][4]
 
-        location_data = get_location_at(response, location)
+        location_data = get_location_at(response, location, reply)
 
-        # return response.reply_with_mention("#{location_data.to_yaml} at #{location}")
-        return response.reply_with_mention("You know what? I can't figure out where \"#{location}\" is, but how about a selfie instead?") if location_data.nil? or location_data.empty?
-        location_data['url'].each do |url|
-          response.reply_with_mention(url)
-        end unless location_data.nil?
+        return response.reply_with_mention("You know what? I can't figure out where \"#{location}\" is, but how about a selfie instead? #selfie") if location_data.nil? or location_data.empty?
+
+        response.reply_with_mention "Looks okay to me!\n#{get_pictures(reply, location_data)}"
       end
 
-      def get_location_at(response, location)
-
-        return if location.nil?
-        response.reply_with_mention "Looks like you're at #yychackathon, the traffic looks alright to me but here are the closest traffic cams" if location.eql? 'city hall'
-        $location_data[location]
+      def get_opts()
+        opts = {}
+        opts['client_id'] = $config_yaml['firestations']['client_id']
+        opts['service_email'] = $config_yaml['firestations']['service_email']
+        opts['key'] = $config_yaml['bigquery']['key']
+        opts['project_id'] = $config_yaml['bigquery']['project_id']
+        opts['dataset'] = $config_yaml['bigquery']['dataset']
+        opts
       end
+
+      def get_location_at(response, location, reply)
+
+        bq = BigQuery::Client.new(get_opts)
+
+        reply << "Looks like you're at #yychackathon, the traffic looks alright to me but here are the closest traffic cams. \n" if location.eql? '11 Avenue'
+
+        data = bq.query("SELECT url FROM [firestations.traffic_cams] where lower(intersection_1) like lower('%#{ location }%') or lower(intersection_2) like lower('%#{ location }%')")
+        return if location.nil? or data['rows'].nil?
+        return data['rows']
+      end
+
+      def get_pictures(reply, location_data)
+        location_data.each do |row|
+          reply << "#{row['f'][0]['v']},"
+        end
+
+        reply.chomp(',')
+      end
+
     end
 
     Lita.register_handler(TrafficCams)
